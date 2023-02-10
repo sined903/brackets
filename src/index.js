77958829b4079d0a9998aebdecf051dbec6113b1
Denis Kulikov <9933denis@gmail.com>
2023-02-10 21:42:28 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let strBracketsConfig = bracketsConfig.map(element => {
+      return element.join('');
+    })
+
+    strBracketsConfig.forEach(element => {
+      let regExp = element;
+      str = str.replaceAll(regExp, '');
+    })
+
+    let isTrue = false;
+
+    strBracketsConfig.forEach(element => {
+      let addSlach = element.split('').map(item => {return '\\' + item}).join('');
+      let regExp;
+      
+      if (isNaN(+element)) {
+        regExp = new RegExp('' + addSlach);
+      } else {
+        regExp = new RegExp('' + element);
+      }
+      if (regExp.test(str)) {
+        isTrue = true;
+      }
+    });
+
+
+    if (isTrue != true && str.length === 0) {
+      return true
+    }  else if (isTrue != true && str.length > 0) {
+      return false
+    } else {
+      return check(str, bracketsConfig)
+    } 
 }
